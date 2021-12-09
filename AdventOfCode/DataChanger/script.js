f2c4ac20f3a70ea3f0ad6eec2f602a2b01cc52a8
DoubleK <kevin-kinner@web.de>
2021-12-09 03:36:37 +0100

    Relocatet Datachanger to seperate site for easier use in the future

---
+++ AdventOfCode/DataChanger/script.js
@@ -0,0 +1,12 @@
+//Reads txt document line by line prints out data for easier conversion to array.
+document.getElementById('inputfile')
+  .addEventListener('change', function () {
+
+  var fr = new FileReader();
+  fr.onload = function () {
+    document.getElementById('output')
+        .textContent = fr.result.split('\n');
+  };
+
+  fr.readAsText(this.files[0]);
+});
