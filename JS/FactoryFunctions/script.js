26850c89f663f60ee47da6dfee0b1cbb542b3e5a
Doubl3K <kevin-kinner@web.de>
2022-07-15 01:18:46 +0200

    Basic FactoryFunctions

--- JS/FactoryFunctions/script.js
+++ JS/FactoryFunctions/script.js
@@ -1,1 +1,34 @@
-console.log("Hello World");
+const Person = (name) => {
+    const sayName = () => console.log(`my name is ${name}`);
+    return { sayName };
+}
+
+const Nerd = (name) => {
+    const { sayName } = Person(name);
+    const doSomethingNerdy = () => console.log('nerd stuff');
+    return { sayName, doSomethingNerdy };
+}
+
+const jeff = Nerd('jeff');
+const torsten = Nerd('torsten');
+
+jeff.sayName();
+jeff.doSomethingNerdy();
+torsten.sayName();
+torsten.doSomethingNerdy();
+
+const proto = {
+    hello() {
+        return `Hello, my name is ${ this.name }`;
+    }
+};
+
+const greeter = (name) => Object.assign(Object.create(proto), {
+    name
+});
+
+const george = greeter('george');
+
+const msg = george.hello();
+
+console.log(msg);
