97824c47f61a54debc9a004f7494b6f9030a6f72
Kevin Kinner <kevin-kinner@web.de>
2024-01-13 22:18:11 +0100

    babel stuuf

--- JS/es6Modules/src/index.js
+++ JS/es6Modules/src/index.js
@@ -7,3 +7,7 @@ let days_since_start_of_month = moment().startOf("month").fromNow();
 h1.innerHTML = days_since_start_of_month;
 body.appendChild(h1);
 console.log(days_since_start_of_month);
+
+var name = "John Doe",
+	time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
