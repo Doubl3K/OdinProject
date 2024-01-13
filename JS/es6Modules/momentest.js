7c8624ff7b888552be7a9a3a4df2ff73a6e19bc7
Kevin Kinner <kevin-kinner@web.de>
2024-01-13 21:36:40 +0100

    webpack with moements in html

--- JS/es6Modules/momentest.js
+++ JS/es6Modules/momentest.js
@@ -1,4 +1,9 @@
 import moment from "moment";
 
+const body = document.querySelector("body");
+console.log(body);
+const h1 = document.createElement("h1");
 let days_since_start_of_month = moment().startOf("month").fromNow();
+h1.innerHTML = days_since_start_of_month;
+body.appendChild(h1);
 console.log(days_since_start_of_month);
